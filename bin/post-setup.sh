f75eb409eb14d7d81c8ad76ad57017f611704bbd
Jacob <soccerplaya5252@yahoo.com>
2021-12-29 11:07:04 -0500

    Updated post-setup.sh to create review app record in SF

--- bin/post-setup.sh
+++ bin/post-setup.sh
@@ -7,4 +7,4 @@ STAGE=${2:-}
 ### This script will execute after the org has been setup
 reviewAppSfdxAuthUrl=$(sfdx force:org:display --verbose -u $TARGET_ORG_ALIAS | grep "Sfdx Auth Url" | awk '{ print $4 }')
 
-invokeCmd "sfdx force:data:record:create -s Review_App__c -u ProdOrg -v \"Review_App_URL='$reviewAppSfdxAuthUrl' Stage__c='$Stage' App__c='$HEROKU_APP_NAME' Branch__c='$HEROKU_BRANCH' Pull_Request__c='$HEROKU_PR_NUMBER'\""
+createReviewAppRecord=$(sfdx force:data:record:create -s Review_App__c -u ProdOrg -v "Review_App_URL='$reviewAppSfdxAuthUrl' Stage__c='$Stage' App__c='$HEROKU_APP_NAME' Branch__c='$HEROKU_BRANCH' Pull_Request__c='$HEROKU_PR_NUMBER'")
